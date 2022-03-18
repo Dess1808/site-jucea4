3ebb45729cb740103a21b75c926c7109b4fe146f
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-03-18 10:48:38 -0400

    primeiro commit da versao 3 do site-jucea

---
+++ README.md
@@ -0,0 +1,16 @@
+Requisicoes alteracao site:
+
+[ ] - Atualizar conteudo (Rede sim) 
+[ ] - Atualizar conteudo da (Implatacao Rede Sim)
+
+[ ] - Colocar Cartilha Digital No site "que cartilha e essa?"
+[x] - sessao "tutoriais" juntando os botoes em 1 so
+
+[ ] - Atualizar conteudo pagina > manuais
+[ ] - Atualizar conteudo PDF > Registro
+[ ] - fazer botao "legislacao" (color tudo que for lesgilacao)
+    - instituicao...
+    - decreto 1.800
+    - lesgilacao rede sim
+    - Rede Sim
+    "onde colocar este botão lesgilacao???"
---
+++ js/carousel.js
@@ -0,0 +1,65 @@
+const track = document.querySelector('.carousel-track')
+const slides = Array.from(track.children)
+const nextButton = document.querySelector('.carousel-button--right')
+const prevButton = document.querySelector('.carousel-button--left')
+const dotsNav = document.querySelector('.carousel-nav')
+const dots = Array.from(dotsNav.children)
+
+const slideWidth = slides[0].getBoundingClientRect().width
+
+//arrange the slider to one another
+const setSlidePosition = (slide, index) =>{
+    slide.style.left = slideWidth * index + 'px'
+}
+
+slides.forEach(setSlidePosition)
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translatex(-' + targetSlide.style.left + ')'
+    currentSlide.classList.remove('current-slide')
+    targetSlide.classList.add('current-slide')
+}
+
+const updateDots = (currentDot, targetDot) =>{
+    currentDot.classList.remove('current-slide')
+    targetDot.classList.add('current-slide')
+}
+
+
+//move left
+prevButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide')
+    const prevSlide = currentSlide.previousElementSibling
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const prevDot = currentDot.previousElementSibling
+
+    moveToSlide(track, currentSlide, prevSlide)
+    updateDots(currentDot, prevDot)
+})
+
+//move right
+nextButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide')
+    const nextSlide = currentSlide.nextElementSibling
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const nextDot = currentDot.nextElementSibling
+
+    moveToSlide(track, currentSlide, nextSlide)
+    updateDots(currentDot, nextDot)
+})
+
+
+//clicando no indicator
+dotsNav.addEventListener('click', e =>{
+    const targetDot = e.target.closest('button')
+
+    if(!targetDot) return
+
+    const currentSlide = track.querySelector('.current-slide')
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const targetIndex = dots.findIndex(dots => dots === targetDot)
+    const targetSlide = slides[targetIndex]
+
+    moveToSlide(track, currentSlide, targetSlide)
+    updateDots(currentDot, targetDot)
+})
